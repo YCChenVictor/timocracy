2216b1a15bd33de4b9f9fd9c6b6d834d9407390e
Victor Chen <victor.chen@hoc-trade.com>
2025-04-30 12:37:34 +0800

    initial

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettierPlugin from "eslint-plugin-prettier";
+import jsonPlugin from "eslint-plugin-json";
+import jestPlugin from "eslint-plugin-jest";
+
+export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  {
+    ignores: ["tsconfig.json"],
+  },
+  {
+    plugins: {
+      prettier: prettierPlugin,
+      jest: jestPlugin,
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "no-console": ["error"],
+      "jest/no-focused-tests": "error",
+    },
+  },
+  {
+    files: ["**/*.json"],
+    ...jsonPlugin.configs.recommended,
+  },
+  {
+    // files: [],
+    // rules: {
+    //   "no-console": "off"
+    // }
+  },
+];
---
+++ src/node.ts
@@ -0,0 +1,9 @@
+import { createLibp2p } from "libp2p";
+import { tcp } from "@libp2p/tcp";
+import { noise } from "@chainsafe/libp2p-noise";
+
+const node = await createLibp2p({
+  transports: [tcp()],
+  connectionEncrypters: [noise()],
+})
+console.log(node.getMultiaddrs())
